b5e7a0f987cb708656573ed81531320c0231ed53
nikimacm <nickymacm@gmail.com>
2021-08-05 10:11:17 +0000

    added flip to memory card in css file and flipCard to js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-const cards=document.querySelectorAll('.memory-card');
-function flipCard(){
-    console.log('I was clicked');
-    console.log('this');
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+  this.classList.toggle('flip');
 }
 
-cards.forEach(card=>card.addEventListener('click',flipCard));
+cards.forEach(card => card.addEventListener('click', flipCard));
