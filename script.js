dcfe161c3846153fc19022f9559f326e963cd6ca
nikimacm <nickymacm@gmail.com>
2021-08-05 11:08:26 +0000

    added dataset object to access the data attribute

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 const cards = document.querySelectorAll('.memory-card');
 
-function flipCard() {
-  this.classList.toggle('flip');
-}
+  let hasFlippedCard = false;
+  let firstCard, secondCard;
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+  function flipCard() {
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+     return;
+   }
+
+   secondCard = this;
+   hasFlippedCard = false;
+
+   checkForMatch();
+ }
+
+ function checkForMatch() {
+   if (firstCard.dataset.name === secondCard.dataset.name) {
+     disableCards();
+     return;
+   }
+
+   unflipCards();
+ }
+
+ function disableCards() {
+   firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+ }
+
+ function unflipCards() {
+   setTimeout(() => {
+     firstCard.classList.remove('flip');
+     secondCard.classList.remove('flip');
+   }, 1500);
+ }
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
